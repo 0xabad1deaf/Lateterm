20668ff324860e32b01b00786bcc79f57fede7d5
Starkaður Hróbjartsson <starkadurh@gmail.com>
2013-11-17 22:48:13 +0000

    Worked some more on Game.java and got some wiked test pasing

--- src/main/java/is/abadidea/tictactoe/Game.java
+++ src/main/java/is/abadidea/tictactoe/Game.java
@@ -6,6 +6,7 @@ public class Game {
 	public Grid grid;
 	// public Player player1;
 	// public Player player2;
+	// public Player currentPlayer;
 
 	public Game(boolean typeOfGame)
 	{
@@ -24,24 +25,24 @@ public class Game {
 	}
 	public boolean gotWinner(){
 		
-		if(grid.matrix[0][0] == grid.matrix[0][1] && grid.matrix[0][0] == grid.matrix[0][2])
+		if(0 != grid.matrix[0][0] && grid.matrix[0][0] == grid.matrix[0][1] && grid.matrix[0][0] == grid.matrix[0][2])
 			return true;
-		if(grid.matrix[0][0] == grid.matrix[1][1] && grid.matrix[0][0] == grid.matrix[2][2])
+		if(0 != grid.matrix[0][0] && grid.matrix[0][0] == grid.matrix[1][1] && grid.matrix[0][0] == grid.matrix[2][2])
 			return true;
-		if(grid.matrix[0][0] == grid.matrix[1][0] && grid.matrix[0][0] == grid.matrix[2][0])
+		if(0 != grid.matrix[0][0] && grid.matrix[0][0] == grid.matrix[1][0] && grid.matrix[0][0] == grid.matrix[2][0])
 			return true;
-		if(grid.matrix[0][1] == grid.matrix[1][1] && grid.matrix[0][1] == grid.matrix[0][2])
+		if(0 != grid.matrix[0][1] && grid.matrix[0][1] == grid.matrix[1][1] && grid.matrix[0][1] == grid.matrix[0][2])
 			return true;
-		if(grid.matrix[0][2] == grid.matrix[1][2] && grid.matrix[0][2] == grid.matrix[2][2])
+		if(0 != grid.matrix[0][2] && grid.matrix[0][2] == grid.matrix[1][2] && grid.matrix[0][2] == grid.matrix[2][2])
 			return true;
-		if(grid.matrix[1][0] == grid.matrix[1][1] && grid.matrix[1][0] == grid.matrix[1][2])
+		if(0 != grid.matrix[1][0] && grid.matrix[1][0] == grid.matrix[1][1] && grid.matrix[1][0] == grid.matrix[1][2])
 			return true;
-		if(grid.matrix[2][0] == grid.matrix[2][1] && grid.matrix[2][0] == grid.matrix[2][2])
+		if(0 != grid.matrix[2][0] && grid.matrix[2][0] == grid.matrix[2][1] && grid.matrix[2][0] == grid.matrix[2][2])
 			return true;
-		if(grid.matrix[0][2] == grid.matrix[1][1] && grid.matrix[0][2] == grid.matrix[2][0])
+		if(0 != grid.matrix[0][2] && grid.matrix[0][2] == grid.matrix[1][1] && grid.matrix[0][2] == grid.matrix[2][0])
 			return true;
-		else
-			return false;
+
+		return false;
 
 	}	
 }
--- src/test/java/is/abadidea/tictactoe/GameTest.java
+++ src/test/java/is/abadidea/tictactoe/GameTest.java
@@ -20,38 +20,66 @@ public class GameTest {
 	public void gotWinnerMethod(){
 		Game game = new Game(true);
 
-		// game.grid.set_x(0,0);
-		// game.grid.set_x(0,1);
-		// game.grid.set_x(0,2);
-		// assertEquals(game.gotWinner(), true);
-		// game.grid.set_x(0,0);
-		// game.grid.set_x(1,1);
-		// game.grid.set_x(2,2);
-		// assertEquals(game.gotWinner(), false);
-		// game.grid.set_x(0,0);
-		// game.grid.set_x(1,0);
-		// game.grid.set_x(2,0);
-		// assertEquals(game.gotWinner(), true);
-		// game.grid.set_x(0,1);
-		// game.grid.set_x(1,1);
-		// game.grid.set_x(0,2);
-		// assertEquals(game.gotWinner(), true);
-		// game.grid.set_x(0,2);
-		// game.grid.set_x(1,2);
-		// game.grid.set_x(2,2);
-		// assertEquals(game.gotWinner(), true);
-		// game.grid.set_x(1,0);
-		// game.grid.set_x(1,1);
-		// game.grid.set_x(1,2);
-		// assertEquals(game.gotWinner(), true);
-		// game.grid.set_x(2,0);
-		// game.grid.set_x(2,1);
-		// game.grid.set_x(2,2);
-		// assertEquals(game.gotWinner(), true);
-		// game.grid.set_x(0,2);
-		// game.grid.set_x(1,1);
-		// game.grid.set_x(2,0);
-		// assertEquals(game.gotWinner(), true);
+
+		//Testing every possible row that resaults in a win
+		game.grid.set_x(0,0);
+		game.grid.set_x(0,1);
+		game.grid.set_x(0,2);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		game.grid.set_y(0,0);
+		game.grid.set_y(1,1);
+		game.grid.set_y(2,2);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		game.grid.set_x(0,0);
+		game.grid.set_x(1,0);
+		game.grid.set_x(2,0);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		game.grid.set_y(0,1);
+		game.grid.set_y(1,1);
+		game.grid.set_y(0,2);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		game.grid.set_x(0,2);
+		game.grid.set_x(1,2);
+		game.grid.set_x(2,2);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		game.grid.set_y(1,0);
+		game.grid.set_y(1,1);
+		game.grid.set_y(1,2);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		game.grid.set_x(2,0);
+		game.grid.set_x(2,1);
+		game.grid.set_x(2,2);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		game.grid.set_y(0,2);
+		game.grid.set_y(1,1);
+		game.grid.set_y(2,0);
+		assertEquals(game.gotWinner(), true);
+		game.grid.clearGrid();
+		//Some combinations that sould not resault in a win
+		game.grid.set_y(0,2);
+		game.grid.set_x(1,1);
+		game.grid.set_y(2,0);
+		assertEquals(game.gotWinner(), false);
+		game.grid.clearGrid();
+		game.grid.set_y(0,2);
+		game.grid.set_y(1,1);
+		game.grid.set_y(2,2);
+		assertEquals(game.gotWinner(), false);
+		game.grid.clearGrid();
+		game.grid.set_y(2,1);
+		game.grid.set_y(1,2);
+		game.grid.set_y(2,2);
+		assertEquals(game.gotWinner(), false);
+		game.grid.clearGrid();
+
+
 
 	}
 }
