c9af48376be71980aa04787ace138bd2e054d827
Kári Helgason <kthelgason@gmail.com>
2013-11-17 22:24:15 +0000

    work on the app

--- src/main/java/is/abadidea/tictactoe/App.java
+++ src/main/java/is/abadidea/tictactoe/App.java
@@ -5,7 +5,7 @@ import spark.*;
 
 public class App 
 {
-    private static char[] matrix;
+    private static Game game;
 
     public static void main( String[] args )
     {
@@ -15,8 +15,35 @@ public class App
             @Override
             public Object handle(Request request, Response response) {
                 response.redirect("/index.html");
-                return "hello";
+                return response;
             }
         });
+
+        get(new Route("/pve") {
+            @Override
+            public Object handle(Request request, Response response) {
+                game = new Game(true);
+                response.redirect("/game.html");
+                return response;
+            }
+        });
+
+        get(new Route("/pvp") {
+            @Override
+            public Object handle(Request request, Response response) {
+                game = new Game(false);
+                response.redirect("/game.html");
+                return response;
+            }
+        });
+
+        get(new Route("/handleClick") {
+            @Override
+            public Object handle(Request request, Response response) {
+                return "{\"message\": \"Hello World\"}";
+            }
+        });
+
     }
+
 }
