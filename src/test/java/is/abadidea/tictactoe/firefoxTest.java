7effba4ed3dc16c706aa1c7924773a76ea1852eb
Benedikt <benedikt12@ru.is>
2013-11-22 19:14:05 +0000

    made some changes in tests

--- src/test/java/is/abadidea/tictactoe/firefoxTest.java
+++ src/test/java/is/abadidea/tictactoe/firefoxTest.java
@@ -4,13 +4,21 @@ import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+import org.openqa.selenium.WebElement;
+import java.util.concurrent.TimeUnit;
+import org.junit.*;
+import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.hamcrest.CoreMatchers.*;
+import org.openqa.selenium.*;
+import org.openqa.selenium.support.ui.Select;
 
 import static junit.framework.Assert.assertTrue;
 
 public class firefoxTest
 {
 	@Test
-	public void driverTest(){
+	public void CheckTitleName(){
 		WebDriver driver = new HtmlUnitDriver(); // a headless driver
 		driver.get("http://tictactoes.co.vu/index.html");
 
@@ -22,10 +30,17 @@ public class firefoxTest
 		WebDriver driver = new FirefoxDriver();
 
 		driver.get("http://tictactoes.co.vu/index.html");
-
 		assertTrue(driver.getTitle().startsWith("Tic Tac Toes!"));
+		//driver.findElement(By.id("playnow!")).click();
+		driver.close();
 	}
 
-
-
+	/*private boolean isElementPresent(By by) {	//function for checking whether element is there
+	    try {
+	      driver.findElement(by);
+	      return true;
+	    } catch (NoSuchElementException e) {
+	      return false;
+	    }
+  	}*/
 }
