d565b5c932724438a5d9c5c7659e4722569a50fb
Jóhann Eiríksson <joiblumen@gmail.com>
2013-11-23 20:13:58 +0000

    Changed selenium test to provide more info to sauce

--- src/test/java/is/abadidea/tictactoe/ITWebDriver.java
+++ src/test/java/is/abadidea/tictactoe/ITWebDriver.java
@@ -1,5 +1,6 @@
 import com.saucelabs.common.SauceOnDemandAuthentication;
 import com.saucelabs.common.SauceOnDemandSessionIdProvider;
+import com.saucelabs.junit.SauceOnDemandTestWatcher;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -11,6 +12,8 @@ import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.openqa.selenium.remote.SessionId;
+
 
 
 
@@ -21,14 +24,27 @@ import static org.junit.Assert.assertTrue;
 
 import com.saucelabs.common.Utils;
 
-/**
- * Simple {@link RemoteWebDriver} test that demonstrates how to run your Selenium tests with <a href="http://saucelabs.com/ondemand">Sauce OnDemand</a>.
- * *
- * @author Ross Rowe
- */
-public class ITWebDriver {
+public class ITWebDriver implements SauceOnDemandSessionIdProvider {
+
+    public SauceOnDemandAuthentication authentication = new SauceOnDemandAuthentication(
+                "joiblumen", "19374220-fef6-4897-8922-fa92f7142053");
+
+    /**
+     * JUnit Rule that marks the Sauce Job as passed/failed when the test succeeds or fails.
+     * You can see the pass/fail status on your [Sauce Labs test page](https://saucelabs.com/tests).
+     */
+    public @Rule
+    SauceOnDemandTestWatcher resultReportingTestWatcher = new SauceOnDemandTestWatcher(this, authentication);
+
+    /**
+     * JUnit Rule that will record the test name of the current test. This is referenced when creating the 
+     * {@link DesiredCapabilities}, so the Sauce Job is created with the test name.
+     */
+    public @Rule TestName testName = new TestName();
 
     private WebDriver driver;
+
+    private String sessionId;
 
     @Before
     public void setUp() throws Exception {
@@ -36,13 +52,18 @@ public class ITWebDriver {
         DesiredCapabilities capabilities = DesiredCapabilities.firefox();
         capabilities.setCapability("version", "25");
         capabilities.setCapability("platform", Platform.WIN8);
+        capabilities.setCapability("name",testName.getMethodName());        
         capabilities.setCapability("tunnel-identifier",Utils.readPropertyOrEnv("TRAVIS_JOB_NUMBER",""));
-        capabilities.setCapability("name",  testName.getMethodName());
         this.driver = new RemoteWebDriver(
-                new URL("http://joiblumen:19374220-fef6-4897-8922-fa92f7142053@ondemand.saucelabs.com:80/wd/hub"),
-                capabilities);
+                new URL("http://" + authentication.getUsername() + ":" + authentication.getAccessKey() + "@ondemand.saucelabs.com:80/wd/hub"), capabilities);
+        this.sessionId = ((RemoteWebDriver)driver).getSessionId().toString();
     }
 
+    @Override
+    public String getSessionId() {
+        return sessionId;
+    }
+    
     @Test
     public void CheckTitleName() throws Exception {
         driver.get("http://localhost:4567");
