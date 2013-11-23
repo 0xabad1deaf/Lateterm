287c1608dded5e2fa67ae7f17cb6d4872e83ebdf
Benedikt <benedikt12@ru.is>
2013-11-23 17:36:18 +0000

    swicth from usin by id to xpath

--- src/test/java/is/abadidea/tictactoe/ITWebDriver.java
+++ src/test/java/is/abadidea/tictactoe/ITWebDriver.java
@@ -10,11 +10,15 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+
 
 
 import java.net.URL;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
 import com.saucelabs.common.Utils;
 
 /**
@@ -47,16 +51,36 @@ public class ITWebDriver {
     @Test
     public void CheckPlayNowButton() throws Exception {
         driver.get("http://localhost:4567");
-        driver.findElement(By.id("playnow")).click();
+        driver.findElement(By.xpath("html/body/div[2]/a/button")).click();
         assertEquals(driver.getCurrentUrl(), "http://localhost:4567/game.html");
     }
 
     @Test
+    public void CheckGrid00() throws Exception {
+        driver.get("http://localhost:4567/game.html");
+        driver.findElement(By.xpath("html/body/div[3]/div[1]/button[1]")).click();
+        driver.findElement(By.xpath("html/body/div[3]/table/tbody/tr[1]/td[1]")).click();
+
+        WebElement a = driver.findElement(By.xpath("html/body/div[3]/table/tbody/tr[1]/td[1]/i"));
+
+
+        //driver.findElement(By.id("box00")).click();
+        //WebElement  i = driver.findElement(By.cssSelector("#box00 fa fa-5x"));
+        WebElement  i = driver.findElement(By.cssSelector("@class fa fa-5x fa-check"));
+        assertTrue(i.isEnabled());
+    }
+
+
+    /*Test
     public void CheckPlayVsComputerButton() throws Exception {
         driver.get("http://localhost:4567/game.html");
         driver.findElement(By.id("btn1")).click();
-        assertEquals(driver.getCurrentUrl(), "http://localhost:4567/game.html");
-    }
+        driver.findElement(By.id("box00")).click();
+
+
+       // WebElement className = driver.findElement(By.cssSelector)
+        assertEquals(driver., "http://localhost:4567/game.html");
+    }*/
 
     @After
     public void tearDown() throws Exception {
